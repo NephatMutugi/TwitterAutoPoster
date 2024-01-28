1b9ddd44319fe4cdcdf1cbd9f3d48d82ae75f54e
Nephat Muchiri <muchirinephat5@gmail.com>
2024-01-28 04:00:42 +0300

    first commit

---
+++ src/main/java/com/neph/main/TwitterAutoPosterApplication.java
@@ -0,0 +1,13 @@
+package com.neph.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAutoPosterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterAutoPosterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neph/main/TwitterAutoPosterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neph.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterAutoPosterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
